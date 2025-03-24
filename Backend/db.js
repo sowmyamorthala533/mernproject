ab7f1adf39bffe0c6d1b12b62f5f6cd352e862fc
sowmyamorthala533 <morthalasowmya77@gmail.com>
2025-03-24 21:28:15 +0530

    first commit

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@
 const mongoose=require("mongoose")
 const connectDB=async()=>{
     try{
-        const conn=await mongoose.connect('mongodb+srv://cwy:test123@cluster0.pdd4z.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0',
+        const conn=await mongoose.connect("mongodb+srv://cwy:test123@cluster0.pdd4z.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0",
            );
         console.log("MongoDB Connected");
     }catch(error){
@@ -11,23 +11,3 @@ const connectDB=async()=>{
     }
 };
 module.exports=connectDB;
-
-
-// // db.js
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     // Replace <db_password> with your actual password
-//     await mongoose.connect("mongodb+srv://cwy:<db_password>@cluster0.pdd4z.mongodb.net/?retryWrites=true&w=majority", {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     });
-//     console.log("MongoDB connected successfully");
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
